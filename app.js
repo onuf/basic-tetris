bd65335340188aa2261812262437657457b88252
onuf <56073962+onuf@users.noreply.github.com>
2020-05-28 10:55:18 +0300

    Tie the selection of a new tetromino to the start button
    The function call responsible for selecting a new tetromino has been moved into the body of the event handler.

--- app.js
+++ app.js
@@ -82,8 +82,6 @@ document.addEventListener('DOMContentLoaded', () => {
         nextRnd = Math.floor(Math.random()*tetrominoes.length)
         curT = tetrominoes[rndT][curRot]
     }
-    
-    setNew()
 
     // draw the tetromino
     function draw() {
@@ -217,6 +215,7 @@ document.addEventListener('DOMContentLoaded', () => {
             timeID = null
         } else {
             // the start option
+            setNew()
             draw()
             // move the tetromino down every second
             timeID = setInterval(moveDown, 1000)
